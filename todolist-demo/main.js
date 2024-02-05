c9fb983413e0138b3773c3ed6157e50954e6ab79
JinKyunJ <148345588+JinKyunJ@users.noreply.github.com>
2024-02-05 14:19:14 +0900

    Todo list_1

---
+++ todolist-demo/main.js
@@ -0,0 +1,125 @@
+// 유저가 값을 입력한다
+// + 버튼을 클릭하면, 할일이 추가된다
+// Delet 버튼을 누르면 할일이 삭제된다
+// Check 버튼을 누르면 할일이 끝나면서 밑줄이 간다
+// 1. Cehck 버튼을 클릭하는 순간 false -> true
+// 2. true이면 끝난걸로 간주하고 밑줄 보여주기
+// 3. false이면 안끝난걸로 간주하고 그대로
+
+// 진행중 완료 탭을 누르면, 언더바가 이동한다
+// 완료탭은 완료한 아이템만, 진행중탭은 진행중인 아이템만
+// 전체탭을 누르면 다시 전체 아이템으로 돌아옴
+
+let taskInput = document.getElementById("task-input");
+let addButton = document.getElementById("add-button");
+let tabs = document.querySelectorAll(".task-tabs div");
+let taskList = [];
+let mode = "all";
+let filterList = [];
+
+addButton.addEventListener("click", addTask);
+
+for (let i = 1; i < tabs.length; i++) {
+  tabs[i].addEventListener("click", function (event) {
+    filter(event);
+  });
+}
+
+function addTask() {
+  let task = {
+    id: randomIDGenerate(),
+    taskContent: taskInput.value,
+    isComplete: false,
+  };
+  taskList.push(task);
+  console.log(taskList);
+  render();
+}
+
+function render() {
+  // 1. 내가 선택한 탭에 따라서
+  let list = [];
+  if (mode === "all") {
+    list = taskList;
+  } else if (mode === "ongoing" || mode === "done") {
+    list = filterList;
+  }
+  // 2. 리스트를 달리 보여준다
+  let resultHTML = "";
+  for (let i = 0; i < list.length; i++) {
+    if (list[i].isComplete == true) {
+      resultHTML += `<div class="task">
+      <div class="task-done">${list[i].taskContent}</div>
+      <div>
+        <button onclick="toggleComplete('${list[i].id}')">Check</button>
+
+        <button onclick="deleteTask'${list[i].id}')">Delete</button>
+      </div>
+    </div>`;
+    } else {
+      resultHTML += `<div class="task">
+    <div>${list[i].taskContent}</div>
+    <div>
+      <button onclick="toggleComplete('${list[i].id}')">Check</button>
+
+      <button onclick="deleteTask('${list[i].id}')">Delete</button>
+    </div>
+  </div>`;
+    }
+  }
+
+  document.getElementById("task-board").innerHTML = resultHTML;
+}
+
+function toggleComplete(id) {
+  for (let i = 0; i < taskList.length; i++) {
+    if (taskList[i].id === id) {
+      taskList[i].isComplete = !taskList[i].isComplete;
+      break;
+    }
+  }
+  render();
+}
+
+function deleteTask(id) {
+  for (let i = 0; i < taskList.length; i++) {
+    if (taskList[i].id == id) {
+      taskList.splice(i, 1);
+      break;
+    }
+  }
+  render();
+}
+
+function filter(event) {
+  console.log("filter", event.target.id);
+  mode = event.target.id;
+  filterList = [];
+  if (mode === "all") {
+    // 전체 리스트를 보여준다
+    render();
+  } else if (mode === "ongoing") {
+    // 진행중인 아이템을 보여준다
+    // task.isComplete = false
+    for (let i = 0; i < taskList.length; i++) {
+      if (taskList[i].isComplete === false) {
+        filterList.push(taskList[i]);
+      }
+    }
+    render();
+    console.log("진행중", filterList);
+  } else if (mode === "done") {
+    // 완료되는 케이스
+    // Task.isComplete = true
+    for (let i = 0; i < taskList.length; i++) {
+      if (taskList[i].isComplete === true) {
+        filterList.push(taskList[i]);
+      }
+    }
+    render();
+  }
+}
+
+function randomIDGenerate() {
+  return "_" + Math.random().toString(36).substring(2, 9);
+}
